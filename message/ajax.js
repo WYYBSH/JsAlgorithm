f09727f1ab1118d531bbe7cec2a6bee90d145db9
unknown <1534995542@qq.com>
2018-10-30 13:59:21 +0800

    ajax

--- message/ajax.js
+++ message/ajax.js
@@ -1,34 +1,65 @@
-function success(text) {
-    var textarea = document.getElementById('test-response-text');
-    textarea.value = text;
-}
-
-function fail(code) {
-    var textarea = document.getElementById('test-response-text');
-    textarea.value = 'Error code: ' + code;
-}
-
-var request = XMLHttpRequest();
-// var request;
-// if (window.XMLHttpRequest) {
-//     request = new XMLHttpRequest();
-// } else {
-//     request = new ActiveXObject('Microsoft.XMLHTTP');
-// }
-
-request.onreadystatechange = function(){
-    if(request.readyState == 4){
-        if (request.status === 200) {
-            // 成功，通过responseText拿到响应的文本:
-            return success(request.responseText);
-        } else {
-            // 失败，根据响应码判断失败原因:
-            return fail(request.status);
-        }
+//注册json格式
+function send_json(){
+    var request;
+    var name = "ddd";
+    var password = "234"
+    var param = {
+        "username": name,
+        "password": password
     }
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+    }  
+    request.onreadystatechange = function(){
+        if(request.readyState == 4){
+            if (request.status === 200) {
+                console.log(request.responseText)
+            } else {
+                console.log("no")
+            }
+        }
+    }   
+    request.open('POST','http://192.168.1.180:8099/user/register');
+    request.setRequestHeader('content-type', 'application/json',true);
+    request.send(JSON.stringify(param));   
+    console.log('请求已经发送，正在等待响应...')
 }
-
-request.open('GET','/api/categories');
-request.send();
-
-alert('请求已经发送，正在等待响应...')
+//登陆 键值对格式
+function login(){
+    var request;
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+   
+    var param = {
+        username: username,
+        password: password
+    }
+    if(typeof param == 'object'){
+        var str = '';
+        for(var key in param){
+            str += key+'='+param[key]+'&';
+        }
+        param = str.replace(/&$/, '');
+    }
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+    }  
+    request.onreadystatechange = function(){
+        if(request.readyState == 4){
+            if (request.status === 200) {
+                console.loh("yes")
+            } else {
+               
+                console.log(request.status)
+            }
+        }
+    }   
+    request.open('POST','http://192.168.1.180:8099/login');
+    request.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    request.send(param);   
+    console.log('请求已经发送，正在等待响应...')
+}
