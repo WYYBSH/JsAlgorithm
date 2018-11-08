b869662d4bf5247fec8ef888dd0cfe2024c6ad04
WYYBSH <1534995542@qq.com>
2018-11-08 22:45:50 +0800

    set

--- ES6/symbol.js
+++ ES6/symbol.js
@@ -1,7 +1,9 @@
 let s = Symbol("hi");
+let s2 = Symbol(1)
 
 //let ss = s.toString();
-console.log(s);
+console.log(s)
+console.log(s2)
 
 //特点：保证每个属性名都是独一无二的。
 //新的数据类型：undefined,null,Boolean,String,Number,Object,Symbol
