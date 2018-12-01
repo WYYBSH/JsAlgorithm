3f7ec3b8805c8ee5da87bcdb1bf80b95e67bcf8a
tsubame <1534995542@qq.com>
2018-12-01 11:44:06 +0800

    promise封装ajax

--- ES6/promise.js
+++ ES6/promise.js
@@ -15,3 +15,38 @@ function loadImage(url) {
         image.src = url
     })
 }
+/*****
+ * promise对象实现ajax的操作
+ */
+const host = 'http://192.168.1.103:8099'
+const url = host + '/creation/template/html/types/card'
+const getJSON = function (url) {
+    const promise = new Promise(function (resolve, reject) {
+        const handler = function () {
+            if (this.readyState !== 4) {
+                return
+            }
+            if (this.status === 200) {
+                resolve(this.response)
+            } else {
+                reject(new Error(this.statusText))
+            }
+        }
+
+        const client = new XMLHttpRequest()
+        client.open("POST", url)
+        client.onreadystatechange = handler
+        client.responseType = "json"
+        client.setRequestHeader("Accept", "application/json")
+        client.setRequestHeader('authorization', tokenVal);
+        client.send()
+    })
+
+    return promise
+}
+
+getJSON(url).then(function (json) {
+    console.log(json)
+}, function (error) {
+    console.log(error)
+})
